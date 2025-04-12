63a724e2af205e11e533c09a20325c99e99e7d5b
“Liliana” <liliana_alvarez25@hotmail.com>
2025-04-12 17:35:10 +0200

    error handling + model, controller, routes of product + category

--- controller/category.js
+++ controller/category.js
@@ -1,1 +1,21 @@
+import Category from "../models/Category.js";
+
 // category controller
+export const getCategories = async (req, res) => {
+    const categories = await Category.findAll();
+    res.json(categories);
+};
+
+export const createCategory = async (req, res) => {
+    try {
+        const {
+            body: { name }
+        } = req;
+        if (!name)
+            return res.status(400).json({ error: 'name is required' });
+        const category = await Category.create(req.body);
+        res.json(category);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
