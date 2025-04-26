d6356abb2339b65e730b7b1b414da5e76e453040
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-04-26 14:10:21 +0200

    added asynchandler

--- routers/orderRoutes.js
+++ routers/orderRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { createOrder, deleteOrder, getOrderById, getOrders, updateOrder } from "../controller/order.js";
+import asyncHandler from "../utils/asyncHandler.js";
 import validateSchama from "../middleware/validateSchema.js";
 import orderSchema from "../schemas/orderSchema.js";
 
@@ -9,13 +10,13 @@ const orderRoutes = Router();
 orderRoutes.get("/", getOrders);
 
 // Create new Order
-orderRoutes.post("/", validateSchama(orderSchema), createOrder);
+orderRoutes.post("/", validateSchama(orderSchema), asyncHandler(createOrder));
 
 // Get Order by ID
 orderRoutes.get("/:id", getOrderById);
 
 // Update Order by ID
-orderRoutes.put("/:id", validateSchama(orderSchema), updateOrder);
+orderRoutes.put("/:id", validateSchama(orderSchema), asyncHandler(updateOrder));
 
 // Delete Order by ID
 orderRoutes.delete("/:id", deleteOrder);
